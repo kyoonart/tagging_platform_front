d4ecc9dbc34068d9ef33c8b3ef7faffa7fb54244
pengtaoa <1564328778@qq.com>
2020-03-25 22:16:20 +0800

    fix bugs

--- src/components/TaggingEntity.vue
+++ src/components/TaggingEntity.vue
@@ -1,28 +1,15 @@
 <template>
 <div>
-    <div class="box1">{{sentence}}
-        <br>
-        <el-tag class="tag" v-for=" (tag,index) in Lsentence" :key="tag" @click="handleSelect(tag,index)">
-            {{tag}}
-        </el-tag>
-        <br>
-        <el-tag v-for="(tag,index) in selected" :key="tag.name" type="success" closable @close="handleClose(tag,index)">
-            {{tag.name}}
-        </el-tag>
-
+    <div class="box1">
+       <input @click="getTextIndex"  v-model="sentence"  style="width:600px"/>
     </div>
     <div class="box2">
         <div style="margin-top: 20px">
-
-            <!-- <el-checkbox-group v-model="asd" :max="1">
-                <el-checkbox-button v-for=" tag in tags" :label="tag.name" :key="tag.name" @change="handleChange(tag)">
-                    {{tag.name}}</el-checkbox-button>
-            </el-checkbox-group> -->
-     <el-radio-group v-model="asd">
-      <el-radio-button  v-for=" tag in tags" :label="tag.name" :key="tag.name" @change="handleChange(tag)">{{tag.name}}</el-radio-button>
-    
+     <el-radio-group v-model="asd" @change="handleChange">
+      <el-radio-button  v-for=" tag in tags" :label="tag.name"  :key="tag.name"  >
+          {{tag.name}}
+          </el-radio-button>
     </el-radio-group>
-
         </div>
     </div>
     <div class="btn">
@@ -53,49 +40,72 @@ export default {
         this.listEntityTypes();
     },
     methods: {
-        handleClose(tag) {
-            console.log(tag);
-            let result = this.selected.filter(item => {
-                return item['id'] !== tag.id
-            })
-            this.selected = result;
-        },
-        handleChange(tag) {
+         getTextIndex (e) {
+            let target = e.target, index;
+            if (target.selectionStart != 'undefined') {
+                  index = target.selectionStart;
+            } else {
+                  index = '0'
+             }        
+            // console.log(index);
+           let selectedText= this.getSelectionText()
+           let end=selectedText.length+index
+           console.log(`start:${index},end : ${end}, selected:${selectedText}`);
+           
+       },
+         getSelectionText() {
+           let selectedText=''
+            if(window.getSelection) {
+            selectedText= window.getSelection().toString();
+            } else if(document.selection && document.selection.createRange) {
+            selectedText= document.selection.createRange().text;
+            }
+             return selectedText;
+        },
+
+        // handleClose(tag) {
+        //     console.log(tag);
+        //     let result = this.selected.filter(item => {
+        //         return item['id'] !== tag.id
+        //     })
+        //     this.selected = result;
+        // },
+        handleChange(value) {
             // 这里可以拿到选中的标
-            console.log(tag);
+            console.log(value);
 
         },
-        handleSelect(tag, index) {
-            // 选到的单词
-            let list = this.sentence.split(' ');
-            let start = 0
-            for (let i = 0; i < index; i++) {
-                start += list[i].length
-            }
-            if (index !== 0) {
-                start = start + index
-            }
-            let pos = {
-                x: start,
-                y: tag.length + start - 1
-            }
-            var name = tag.replace(/[, , .]/g, "")
-            var obj = {
-                id: index,
-                name: name
-            }
-            let count = 0
-            for (let i = 0; i < this.selected.length; i++) {
-                console.log(this.selected[i].id);
-                if (this.selected[i].id === index) {
-                    this.$message.error('请勿重复选择');
-                    count++
-                }
-            }
-            if (count === 0) {
-                this.selected.push(obj);
-            }
-        },
+        // handleSelect(tag, index) {
+        //     // 选到的单词
+        //     let list = this.sentence.split(' ');
+        //     let start = 0
+        //     for (let i = 0; i < index; i++) {
+        //         start += list[i].length
+        //     }
+        //     if (index !== 0) {
+        //         start = start + index
+        //     }
+        //     let pos = {
+        //         x: start,
+        //         y: tag.length + start - 1
+        //     }
+        //     var name = tag.replace(/[, , .]/g, "")
+        //     var obj = {
+        //         id: index,
+        //         name: name
+        //     }
+        //     let count = 0
+        //     for (let i = 0; i < this.selected.length; i++) {
+        //         console.log(this.selected[i].id);
+        //         if (this.selected[i].id === index) {
+        //             this.$message.error('请勿重复选择');
+        //             count++
+        //         }
+        //     }
+        //     if (count === 0) {
+        //         this.selected.push(obj);
+        //     }
+        // },
         handleSave() {
             // let query={
             //   sentence_id:
@@ -114,7 +124,7 @@ export default {
             let post_data = { referer: "entity" };
             const resp = await this.$http.post('/Sentence/Get', post_data)
             if (resp.data.success) {
-                console.log(resp.data.data);
+                // console.log(resp.data.data);
 
                 this.sentence = resp.data.data[0].content;
                 this.Lsentence = resp.data.data[0].content.split(' ')
@@ -152,6 +162,10 @@ export default {
 .tag {
     margin-top: 20px;
     margin-right: 2px
+}
+input {
+    width: 1000px!important;
+    height: 40px
 }
 
 </style>
